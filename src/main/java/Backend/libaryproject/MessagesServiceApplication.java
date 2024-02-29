25d5f34df91676573e7ed2d171502ccd033ade88
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2024-02-29 00:14:11 +0000

    Start

--- src/main/java/Backend/libaryproject/MessagesServiceApplication.java
+++ src/main/java/Backend/libaryproject/MessagesServiceApplication.java
@@ -2,10 +2,12 @@ package Backend.libaryproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients //This is to enable feign clients
+@EnableDiscoveryClient
 public class MessagesServiceApplication {
 
     public static void main(String[] args) {
